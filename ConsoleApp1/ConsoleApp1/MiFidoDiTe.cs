d9769b3445d0326cbbe0f9c05f2273e185c2c295
Mauro Guagliardo <mauro.guagliardo@insiel.it>
2022-03-03 11:48:31 +0100

    Aggiunta MiFido

---
+++ ConsoleApp1/ConsoleApp1/MiFidoDiTe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MiFidoDiTe
+    {
+        public int ID { get; set; }
+        public string Descrizione { get; set; } = string.Empty;
+    }
+}
