63fcfe4ab752a2430771c67b80f6620ca802a404
Mauro Guagliardo <mauro.guagliardo@insiel.it>
2022-03-03 12:11:25 +0100

    Creata PersonalTask

---
+++ ConsoleApp1/ConsoleApp1/PersonalTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PersonalTask
+    {
+        public int ID { get; set; }
+        public string DescriptionTask { get; set; } = string.Empty;
+        public int Priority { get; set; }
+    }
+}
